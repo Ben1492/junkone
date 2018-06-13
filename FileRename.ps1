6a5dce493c87a3f63470bdfd84056a02a74ae2d4
Ben1492 <redkadett@gmail.com>
2018-06-12 18:20:02 -0700

    added some comments at the end

--- FileRename.ps1
+++ FileRename.ps1
@@ -6,3 +6,7 @@ for ($i = 0; $i -lt $files.count; $i++) {
     $fileName = "C:\Users\Ben\Documents\PS Scripts\FileRename\FinalFile" + ($i+1)+".txt"
     Rename-Item $files[$i].FullName $fileName
 }
+
+<#this is a block
+of comment code
+just to add some changes to the file#>
